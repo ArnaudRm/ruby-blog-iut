da4a0a16819b369a2c7e0a879adb6fcce2c61206
Arnaud Ayrem <arnaud.ayrem@gmail.com>
2018-07-05 23:34:56 +0200

    Implemented basic user MVC

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.order(:lastname).all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to users_path
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @user.update_attributes user_params
+      redirect_to user_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :avatar)
+  end
+
+end
