dd17ca4120639fc2bc5c83d94439ffae8abca88c
Arnaud Ayrem <arnaud.ayrem@gmail.com>
2018-07-14 17:35:04 +0200

    Added manually timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_14_152150) do
+ActiveRecord::Schema.define(version: 2018_07_14_153058) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.string "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
